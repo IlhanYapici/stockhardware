af1bc380fa9c4b6eb5f2ea8968d6b8abf77676b0
IlhanYapici <ilhan-yapici@outlook.fr>
2022-05-02 23:23:26 +0200

    Moving chakra provider from 'src/App.jsx' to 'src/main.jsx'

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,41 +1,37 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ChakraProvider, theme } from '@chakra-ui/react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ColorModeScript } from "@chakra-ui/react";
+import axios from "axios";
 
-import './app.css';
+import "./app.css";
 
-import Navbar from './components/navbar/navbar';
-import MainPage from './pages/main/mainPage';
-import LandingPage from './pages/landing/landingPage';
-import GpuPage from './pages/gpu/gpuPage';
-import CpuPage from './pages/cpu/cpuPage';
-import LoginPage from './pages/login/loginPage';
-import SignupPage from './pages/signup/signupPage';
+import Navbar from "./components/navbar/navbar";
+import LandingPage from "./pages/landing/landingPage";
+import GpuPage from "./pages/gpu/gpuPage";
+import CpuPage from "./pages/cpu/cpuPage";
+import LoginPage from "./pages/login/loginPage";
+import SignupPage from "./pages/signup/signupPage";
 // - - - - - - - -
 // Admin section
-import AdminPanel from './pages/adminPanel/adminPanel';
-import UserPanel from './components/adminPanel/userPanel/userPanel';
-import ViewUsers from './components/adminPanel/userPanel/viewUsers';
-import UserDetails from './components/adminPanel/userPanel/userDetails';
-import EditUser from './components/adminPanel/userPanel/editUser';
-import AddUser from './components/adminPanel/userPanel/addUser';
-import GpuPanel from './components/adminPanel/gpuPanel/gpuPanel';
-import CpuPanel from './components/adminPanel/cpuPanel/cpuPanel';
+import AdminPanel from "./pages/adminPanel/adminPanel";
+import UserPanel from "./components/adminPanel/userPanel/userPanel";
+import ViewUsers from "./components/adminPanel/userPanel/viewUsers";
+import UserDetails from "./components/adminPanel/userPanel/userDetails";
+import EditUser from "./components/adminPanel/userPanel/editUser";
+import AddUser from "./components/adminPanel/userPanel/addUser";
+import GpuPanel from "./components/adminPanel/gpuPanel/gpuPanel";
+import CpuPanel from "./components/adminPanel/cpuPanel/cpuPanel";
 
 function App() {
-  const [firstname, setFirstname] = useState('');
-  const [lastname, setLastname] = useState('');
+  const [firstname, setFirstname] = useState("");
+  const [lastname, setLastname] = useState("");
   const [isAdmin, setIsAdmin] = useState(false);
 
   useEffect(() => {
     const getUser = async () => {
-      const { user } = JSON.parse(localStorage.getItem('userInfo'));
+      const { user } = JSON.parse(localStorage.getItem("userInfo"));
       const bearer = `Bearer ${user}`;
-      const url =
-          document.location.hostname === 'localhost'
-              ? `http://localhost:5000/users/${user}`
-              : `http://10.0.0.11:5000/users/${user}`;
+      const url = `http://10.0.0.3:5000/users/${user}`;
 
       const { data } = await axios.get(url, {
         headers: { Authorization: bearer },
@@ -45,64 +41,62 @@ function App() {
       setIsAdmin(data.isAdmin);
     };
 
-    if (localStorage.getItem('userInfo'))
-      getUser().catch(err => {
-        localStorage.removeItem('userInfo');
+    if (localStorage.getItem("userInfo"))
+      getUser().catch((err) => {
+        localStorage.removeItem("userInfo");
         window.location.reload();
       });
   }, []);
 
   return (
-      <ChakraProvider theme={theme}>
-        <Router>
-          <Routes>
-            <Route
-                path="*"
-                element={
-                  <Navbar
-                      links={['Home', 'GPU', 'CPU']}
-                      isAdmin={isAdmin}
-                      firstname={firstname}
-                      lastname={lastname}
-                  />
-                }
+    <Router>
+      <Routes>
+        <Route
+          path="*"
+          element={
+            <Navbar
+              links={["Home", "GPU", "CPU"]}
+              isAdmin={isAdmin}
+              firstname={firstname}
+              lastname={lastname}
             />
-            <Route
-                path="/"
-                element={
-                  <Navbar
-                      links={['Home', 'GPU', 'CPU']}
-                      isAdmin={isAdmin}
-                      firstname={firstname}
-                      lastname={lastname}
-                  />
-                }
-            >
-              <Route index element={<LandingPage />} />
-              <Route path="gpu" element={<GpuPage />} />
-              <Route path="cpu" element={<CpuPage />} />
-              <Route path="login" element={<LoginPage />} />
-              <Route path="signin" element={<SignupPage />} />
-            </Route>
-            <Route path="admin-panel" element={<AdminPanel isAdmin={isAdmin} />}>
-              <Route path="users" element={<UserPanel />}>
-                <Route index element={<ViewUsers />} />
-                <Route path="edit/:userId" element={<EditUser />} />
-                <Route path="add" element={<AddUser />} />
-                <Route path=":userId" element={<UserDetails />} />
-              </Route>
-              <Route path="gpus" element={<GpuPanel />}>
-                <Route path="edit/:gpuId" element={<></>} />
-                <Route path=":gpuId" element={<></>} />
-              </Route>
-              <Route path="cpus" element={<CpuPanel />}>
-                <Route path="edit/:cpuId" element={<></>} />
-                <Route path=":cpuId" element={<></>} />
-              </Route>
-            </Route>
-          </Routes>
-        </Router>
-      </ChakraProvider>
+          }
+        />
+        <Route
+          path="/"
+          element={
+            <Navbar
+              links={["Home", "GPU", "CPU"]}
+              isAdmin={isAdmin}
+              firstname={firstname}
+              lastname={lastname}
+            />
+          }
+        >
+          <Route index element={<LandingPage />} />
+          <Route path="gpu" element={<GpuPage />} />
+          <Route path="cpu" element={<CpuPage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="signin" element={<SignupPage />} />
+        </Route>
+        <Route path="admin-panel" element={<AdminPanel isAdmin={isAdmin} />}>
+          <Route path="users" element={<UserPanel />}>
+            <Route index element={<ViewUsers />} />
+            <Route path="edit/:userId" element={<EditUser />} />
+            <Route path="add" element={<AddUser />} />
+            <Route path=":userId" element={<UserDetails />} />
+          </Route>
+          <Route path="gpus" element={<GpuPanel />}>
+            <Route path="edit/:gpuId" element={<></>} />
+            <Route path=":gpuId" element={<></>} />
+          </Route>
+          <Route path="cpus" element={<CpuPanel />}>
+            <Route path="edit/:cpuId" element={<></>} />
+            <Route path=":cpuId" element={<></>} />
+          </Route>
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 
