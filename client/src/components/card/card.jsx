80b2a4eddf549829042240aaf42e916d9d631bcb
IlhanYapici <ilhan-yapici@outlook.fr>
2022-05-25 11:43:11 +0200

    clickable card

--- client/src/components/card/card.jsx
+++ client/src/components/card/card.jsx
@@ -1,12 +1,30 @@
-import { Flex, Stat, StatNumber, StatLabel } from '@chakra-ui/react';
+import {
+  Flex,
+  LinkBox,
+  LinkOverlay,
+  Stat,
+  StatNumber,
+  StatLabel,
+} from "@chakra-ui/react";
+import { useNavigate } from "react-router-dom";
 
-const Card = ({ title, price }) => {
+const Card = ({ itemId, title, price }) => {
+  const navigate = useNavigate();
+
   return (
-    <Flex flexDirection="column" w="250px" h="250px">
-      <Stat>
-        <StatLabel>{title}</StatLabel>
-        <StatNumber>${price}</StatNumber>
-      </Stat>
+    <Flex flexDirection="column" w="250px" h="250px" backgroundColor="gray.700">
+      <LinkBox>
+        <LinkOverlay
+          zIndex="1"
+          onClick={() => navigate(itemId)}
+          _hover={{ cursor: "pointer" }}
+        >
+          <Stat>
+            <StatLabel>{title}</StatLabel>
+            <StatNumber>${price}</StatNumber>
+          </Stat>
+        </LinkOverlay>
+      </LinkBox>
     </Flex>
   );
 };
