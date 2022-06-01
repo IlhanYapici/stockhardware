2f3ada0f96023971b6297e2c5de5231cbc18a344
Ilhan Yapici <ilhan-yapici@outlook.fr>
2022-06-01 15:14:16 +0200

    layout fix

--- client/src/pages/landing/landingPage.jsx
+++ client/src/pages/landing/landingPage.jsx
@@ -1,9 +1,9 @@
-import { Heading, Flex } from '@chakra-ui/react';
-import { Outlet } from 'react-router-dom';
+import { Heading, Flex } from "@chakra-ui/react";
+import { Outlet } from "react-router-dom";
 
 const LandingPage = () => {
   return (
-    <Flex justifyContent="center">
+    <Flex justifyContent="center" pt="55px">
       <Heading>Welcome to StockHardware!</Heading>
       <Outlet />
     </Flex>
