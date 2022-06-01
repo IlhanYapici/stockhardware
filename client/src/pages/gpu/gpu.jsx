7d76190caa0a0bee3c22a2da769a7a012a80c783
Ilhan Yapici <ilhan-yapici@outlook.fr>
2022-06-01 15:13:16 +0200

    layout fix

--- client/src/pages/gpu/gpu.jsx
+++ client/src/pages/gpu/gpu.jsx
@@ -2,7 +2,7 @@ import { Flex } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 
 const GpuLanding = () => (
-  <Flex id="gpu__landing" w="100%">
+  <Flex id="gpu__landing" w="100%" pt="55px">
     <Outlet />
   </Flex>
 );
