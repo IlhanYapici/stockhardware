0b0c8e29dbccf5ad75e7a8fab51647eb5742c4de
Ilhan Yapici <ilhan-yapici@outlook.fr>
2022-06-01 15:14:00 +0200

    updated file name

--- client/src/pages/gpu/gpuPage.jsx
+++
@@ -1,9 +0,0 @@
-import { Heading } from "@chakra-ui/react";
-import { useParams } from "react-router-dom";
-
-const GpuPage = () => {
-  const { gpuId } = useParams();
-  return <Heading>{gpuId}</Heading>;
-};
-
-export default GpuPage;
